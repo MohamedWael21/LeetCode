cb384c3c782e3d195cea59c5be082d897c731f29
Mohamed Wael <mohame171819@gmail.com>
2026-01-15 10:40:11 +0200

    solve 739. Daily Temperatures

---
+++ 081/739. Daily Temperatures.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int>ans(temperatures.size());
+        stack<int>st;
+        for(int i=temperatures.size()-1; i>=0; i--){
+            while(!st.empty() && temperatures[st.top()] <= temperatures[i])
+                st.pop();
+            if(!st.empty())
+                ans[i] = st.top()-i;
+            else 
+                ans[i] = 0;
+            st.push(i);
+        }
+        return ans;
+    }
+};
