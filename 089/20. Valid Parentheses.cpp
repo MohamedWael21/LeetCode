0dd0290256942f8e029aaf1fb852a4b5baa2bce2
Mohamed Wael <mohame171819@gmail.com>
2026-01-18 08:47:49 +0200

    solve 20. Valid Parentheses

---
+++ 089/20. Valid Parentheses.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(char c : s){
+            if(c == '(' || c == '{' || c == '[')
+                st.push(c);
+            if((c == ')' || c == ']' || c == '}') && st.empty()) return false;
+            if(c == ')' && st.top() != '(') return false;
+            if(c == '}' && st.top() != '{') return false;
+            if(c == ']' && st.top() != '[') return false;
+            if(c == ')' || c == ']' || c == '}') st.pop();
+        }
+        return st.empty();
+    }
