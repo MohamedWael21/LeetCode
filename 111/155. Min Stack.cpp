1e9cc98389aebb09f0b3f06c40eeab6bfcf2978e
Mohamed Wael <mohame171819@gmail.com>
2026-01-27 09:54:07 +0200

    solve 155. Min Stack

---
+++ 111/155. Min Stack.cpp
@@ -0,0 +1,38 @@
+class MinStack {
+    vector<int>mins;
+    vector<int>st;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        st.push_back(val);
+        if(mins.empty() || val <= mins.back())
+            mins.push_back(val);
+    }
+    
+    void pop() {
+        if(!mins.empty() && st.back() == mins.back()){
+            mins.pop_back();
+        }
+        st.pop_back();
+    }
+    
+    int top() {
+        return st.back();
+    }
+    
+    int getMin() {
+        return mins.back();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
