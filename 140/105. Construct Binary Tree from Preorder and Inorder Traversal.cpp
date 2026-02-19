aa2e3ebe49781d2cad846170259169d0cdaa759a
Mohamed Wael <mohame171819@gmail.com>
2026-02-19 11:34:38 +0200

    solve 105. Construct Binary Tree from Preorder and Inorder Traversal

---
+++ 140/105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        map<int, int>mp;
+        for(int i=0; i<inorder.size(); i++){
+            mp[inorder[i]] = i;
+        }
+        int i = 0;
+        function<TreeNode*(int, int)> sv = [&](int s, int e) -> TreeNode*{
+            if(s>e) return nullptr;
+            TreeNode* curr = new TreeNode();
+            int pos = mp[preorder[i++]];
+            curr->val = inorder[pos];
+            curr->left = sv(s, pos-1);
+            curr->right = sv(pos+1, e);
+            return curr;
+        };
+        TreeNode* ans = sv(0, inorder.size()-1);
+        return ans;
+    }
+};
