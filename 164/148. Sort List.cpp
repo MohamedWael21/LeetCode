b4a1c8f84aa6e2d1309e6b0afdffa33945b44570
Mohamed Wael <mohame171819@gmail.com>
2026-03-19 12:53:56 +0200

    solve 148. Sort List

---
+++ 164/148. Sort List.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeSort(ListNode* head){
+        if(!head || !head->next) return head;
+        ListNode* prev = nullptr;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next){
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        prev->next = nullptr;
+        ListNode* left = mergeSort(head);
+        ListNode* right = mergeSort(slow);
+        return merge(left, right);
+    }
+    ListNode* merge(ListNode* l1, ListNode* l2){
+        ListNode* dummy = new ListNode();
+        ListNode* curr = dummy;
+        while(l1 && l2){
+            if(l1->val < l2->val){
+                curr->next = l1;
+                l1 = l1->next;
+            }else{
+                curr->next = l2;
+                l2 = l2->next;
+            }
+            curr = curr->next;
+        }
+        if(l1) curr->next = l1;
+        if(l2) curr->next = l2;
+        return dummy->next;
+    }
+    ListNode* sortList(ListNode* head) {
+        return mergeSort(head);
+    }
+};
