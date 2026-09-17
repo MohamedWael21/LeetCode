7550a25a54bba8774f0444ed646b5a2cb1d0ea75
Mohamed Wael <mohame171819@gmail.com>
2026-09-17 10:33:20 +0300

    solve 9. Palindrome Number

---
+++ 183/9. Palindrome Number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        long long b;
+        long long m = x;
+        long long y = 0;
+
+        while(x){
+            y = y*10 + (x%10);
+            x /= 10;
+        }
+
+        return y==m;
+    }
+};
+
